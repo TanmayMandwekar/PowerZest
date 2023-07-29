6b9cec9d672eff16ca77040e57839fc1c6f8caff
Tanmay Mandwekar <136910963+TanmayMandwekar@users.noreply.github.com>
2023-07-29 10:18:08 +0530

    files change

---
+++ index.js
@@ -0,0 +1,6 @@
+<script language="JavaScript">
+  var frmvalidator = new Validator("workform");
+  frmvalidator.addValidation("email","req","Please provide your email");
+  frmvalidator.addValidation("email","email", "Please enter a valid email
+  address");
+</script>;
